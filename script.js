b31cd921e408108df757ac7bd1b43795262a1b52
vic-74 <victormirandafernandes574@gmail.com>
2025-09-16 12:01:37 -0300

    a

---
+++ script.js
@@ -0,0 +1,12 @@
+const sons = {
+    'A' : 'boom.wav',
+    'S' : 'clap.wav',
+    'D' : 'hithat.wav',
+    'F' : 'hithat.wav',
+    'G' : 'kick.wav',
+    'H' : 'openhat.wav',
+    'I' : 'ride.wav',
+    'J' : 'snare.wav',
+    'K' : 'tink.wav',
+    'L' : 'tom.wav',
+}
